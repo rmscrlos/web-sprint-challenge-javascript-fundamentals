2beb9e6e841f88123a323eaee5afafeb5e747302
Jean Carlos  Ramos <rmscrlos@gmail.com>
2020-09-12 13:07:13 -0400

    finished closures challanges

--- challenges/closure.js
+++ challenges/closure.js
@@ -16,9 +16,22 @@ function myFunction() {
 }
 myFunction();
 
-// Explanation: 
+// Explanation: Closure gives the ability to only reach out to the parent function and grab a variable to bring inside its block scope. That is why the nestedFunction was able to console.log(internal) because closure is happening there. 
 
 
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+
+function summation(num){
+  counter = 0;
+  function sum(){
+    for(let i = 0; i <= num; i++){
+      counter += num - 2;
+    }
+  }
+  sum();
+}
+summation(4);
+console.log(counter);
+
